887aca7242c80bfb6e28bbbbd1268f999b3ea05b
Egunov Yaroslav <antonioshmonderos@gmail.com>
2025-03-18 22:53:29 +0700

    Сколько дитэйлс...

--- Habitualize/Habitualize/View/AppProfile.xaml.cs
+++ Habitualize/Habitualize/View/AppProfile.xaml.cs
@@ -4,12 +4,17 @@ using Firebase.Auth.Providers;
 using Firebase.Auth.Repository;
 using Habitualize.SignPages;
 using Habitualize.View;
+using Microsoft.Maui;
+using Microsoft.Maui.Media;
+using Microsoft.Maui.Storage;
 
 namespace Habitualize;
 
 public partial class AppProfile : ContentPage
 {
     public string Username { get; }
+
+    public string Bio { get; set; }
 
     public int Age { get; set; }
 
@@ -20,13 +25,64 @@ public partial class AppProfile : ContentPage
 		InitializeComponent();
         NavigationPage.SetHasNavigationBar(this, false);
         Username = Preferences.Get("Username", "Default Username");
+        Bio = Preferences.Get("UserBio", "Enter a short biography...");
+
+        var avatarPath = Preferences.Get("UserAvatar", string.Empty);
+        if (!string.IsNullOrEmpty(avatarPath))
+        {
+            var imageSource = ImageSource.FromFile(avatarPath);
+            var avatarImage = this.FindByName<Image>("AvatarImage");
+            avatarImage.Source = imageSource;
+        }
+
         BindingContext = this;
     }
 
-    private void OnAvatarTapped(object sender, EventArgs e)
+    private async void OnAvatarTapped(object sender, EventArgs e)
     {
-        // Обработчик события нажатия на аватар
-        DisplayAlert("Аватар", "Аватар был нажат", "OK");
+        try
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Select a photo"
+            });
+
+            if (result != null)
+            {
+                Preferences.Set("UserAvatar", result.FullPath);
+
+                var stream = await result.OpenReadAsync();
+                var imageSource = ImageSource.FromStream(() => stream);
+                var image = (Image)sender;
+                image.Source = imageSource;
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"Failed to select photo: {ex.Message}", "OK");
+        }
+    }
+
+    private void OnBioTextChanged(object sender, TextChangedEventArgs e)
+    {
+        Preferences.Set("UserBio", e.NewTextValue);
+    }
+
+    private void OnBioEditorFocused(object sender, FocusEventArgs e)
+    {
+        BioEditor.IsReadOnly = false;
+    }
+
+    private void OnBioEditorUnfocused(object sender, FocusEventArgs e)
+    {
+        BioEditor.IsReadOnly = true;
+        Preferences.Set("UserBio", BioEditor.Text);
+    }
+
+    private async void OnAchievementTapped(object sender, EventArgs e)
+    {
+        var description = (string)((TappedEventArgs)e).Parameter;
+        await DisplayAlert("Achivment: ", description, "OK");
     }
 
     private async void OnSettingsButtonClicked(object sender, EventArgs e)
@@ -53,4 +109,9 @@ public partial class AppProfile : ContentPage
     {
         await Navigation.PushAsync(new MainPage());
     }
+}
+public class Achievement
+{
+    public string Icon { get; set; }
+    public string Description { get; set; }
 }
--- Habitualize/Habitualize/View/AppSettings.xaml.cs
+++ Habitualize/Habitualize/View/AppSettings.xaml.cs
@@ -12,6 +12,15 @@ public partial class AppSettings : ContentPage
         NavigationPage.SetHasNavigationBar(this, false);
         BindingContext = viewModel;
     }
+    private async void OnPrivacyPolicyClicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync(new Uri("https://drive.google.com/file/d/1VIKkbDjYDUNsH8zoGkf29vU9N56H9-5e/view?usp=sharing"));
+    }
+
+    private async void OnTermsAndConditionsClicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync(new Uri("https://drive.google.com/file/d/14zHOxYc0s_7LxC62gaEheVa2AY0pUUxu/view?usp=sharing"));
+    }
 
     private async void OnMapButtonClicked(object sender, EventArgs e)
     {
