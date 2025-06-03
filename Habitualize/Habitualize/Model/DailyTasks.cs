ea2c2d8745d0987e846498ebcb10bc63f5088c2b
AgentKatya <misterghostwork@gmail.com>
2025-06-03 16:01:51 +0700

    Сделал классы для прогрессии и для дейликов

---
+++ Habitualize/Habitualize/Model/DailyTasks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Habitualize.Model
+{
+    public class DailyTasks
+    {
+        public string TaskName { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public Func<HabitTemplate, bool> Condition { get; set; }
+
+        public bool IsTaskComplete(HabitTemplate habit)
+        {
+            return Condition(habit);
+        }
+    }
+}
---
+++ Habitualize/Habitualize/Model/ProgressionSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Habitualize.Model
+{
+    public static class ProgressionSystem
+    {
+        public static int Experience { get; set; } = 0;
+
+        public static int ExpForLevel { get; set; } = 100;
+
+        public static int Level { get; set; } = 1;
+
+        public static bool IsLevelUp()
+        {
+            if(Experience >= ExpForLevel)
+            {
+                Level++;
+                Experience = 0;
+                ExpForLevel += 50;
+                return true;
+            }
+            return false;
+        }
+    }
+}
